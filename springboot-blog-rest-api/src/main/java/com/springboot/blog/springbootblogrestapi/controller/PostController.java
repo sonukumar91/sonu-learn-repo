81604217ef53955fdd9358b0aa66302ce133f6a0
Sonu Mahto <sonukumarmahto91@gmail.com>
2024-02-08 18:53:05 +0530

    Today commit like Spring Security things

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostController.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ public class PostController {
 	private PostService postService;
 
 	// create blog post
+	@PreAuthorize("hasRole('ADMIN')")
 	@PostMapping
 	public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
 		return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
@@ -42,12 +44,14 @@ public class PostController {
 		return ResponseEntity.ok(postService.getPostById(id));
 	}
 
+	@PreAuthorize("hasRole('ADMIN')")
 	@PutMapping("/{id}")
 	public ResponseEntity<PostDto> updatePostById(@Valid @RequestBody PostDto postDto,
 			@PathVariable(name = "id") long id) {
 		return new ResponseEntity<PostDto>(postService.updatePost(postDto, id), HttpStatus.OK);
 	}
 
+	@PreAuthorize("hasRole('ADMIN')")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String> deletePostById(@PathVariable(name = "id") long id) {
 
---
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/entity/Role.java
@@ -0,0 +1,23 @@
+package com.springboot.blog.springbootblogrestapi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="roles")
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+}
