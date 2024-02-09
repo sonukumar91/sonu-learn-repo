d189185237f4d5640d7b7e6522052c1ab4104660
Sonu Mahto <sonukumarmahto91@gmail.com>
2024-02-09 18:50:32 +0530

    Category Entity Added

---
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/entity/Category.java
@@ -0,0 +1,32 @@
+package com.springboot.blog.springbootblogrestapi.entity;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String description;
+	
+	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Post> posts; 
+
+}
